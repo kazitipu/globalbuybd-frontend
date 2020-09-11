e99febefa29ded67d537b47727203237bb5b9ee7
kazitipu <kazi.tipu.nxt@gmail.com>
2020-09-12 03:09:55 +0600

    added build

---
+++ build/precache-manifest.d6f7a759111ef4acfc4141e0205dbee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c1bd96dc3ee7e74ba6",
+    "url": "/static/css/main.aef21d02.chunk.css"
+  },
+  {
+    "revision": "e3c1bd96dc3ee7e74ba6",
+    "url": "/static/js/main.e3c1bd96.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "2a5e963d4aaef145399d",
+    "url": "/static/css/2.131a0648.chunk.css"
+  },
+  {
+    "revision": "2a5e963d4aaef145399d",
+    "url": "/static/js/2.2a5e963d.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "852853cbe7205dc2ae32b1503845a6f8",
+    "url": "/index.html"
+  }
+];
